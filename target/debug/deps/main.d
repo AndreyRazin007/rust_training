741aac65297dd24ad6c69aab33848f59ad340306
AndreyRazin <programingislife@gmail.com>
2023-01-15 13:53:07 +0500

    Added new files

---
+++ target/debug/deps/main.d
@@ -0,0 +1,5 @@
+A:\Programing\Rust\rust_training\target\debug\deps\main.exe: src\main.rs
+
+A:\Programing\Rust\rust_training\target\debug\deps\main.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/main.d
@@ -0,0 +1,1 @@
+A:\Programing\Rust\rust_training\target\debug\main.exe: A:\Programing\Rust\rust_training\src\main.rs
